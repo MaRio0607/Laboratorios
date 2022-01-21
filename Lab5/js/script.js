fc9bf2416d4e89b15fb5451ecb1297d07bffb257
Mario <57414832+MaRio0607@users.noreply.github.com>
2022-01-21 12:21:31 -0600

    lab 5 done

--- Lab5/js/script.js
+++ Lab5/js/script.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
 
     $(".submitButton").on('click', addNmae)
+    $("#ButtonDelete").on('click', deleteAll)
+    $("#ButtonClear").on('click', clearAll)
+    $("#ButtonMark").on('click', markAll)
+
+
 
     function addNmae(e){
         e.preventDefault();
@@ -15,5 +20,30 @@ $(document).ready(function(){
         }
     };
 
+    function deleteAll(){
+        let todos = document.getElementsByName("todo")
+        for(let i = 0; i< todos.length; i++){
+            if (todos[i].checked){
+                //todos[i].parentElement.remove()
+                $(todos[i]).parent().remove()
+            }
+        }
+    }
+
+    function clearAll(){
+        let todos = document.getElementsByName("todo")
+        for(let i = 0; i< todos.length; i++){
+            todos[i].checked = false
+        }
+    }
+
+    function markAll(){
+        let todos = document.getElementsByName("todo")
+        for(let i = 0; i< todos.length; i++){
+            todos[i].checked = true
+        }
+    }
+
+  
 
 });
