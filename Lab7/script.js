356a24c12e831a627ffeb2e9942533901afca5dc
Mario <57414832+MaRio0607@users.noreply.github.com>
2022-01-24 19:29:58 -0600

    add but with Gif

--- Lab7/script.js
+++ Lab7/script.js
@@ -1,26 +1,48 @@
 $(document).ready(function() {
 
 // Start your code from here
-var tema=[
-    "France","Spain","Germany","Sweden","Denmark"
-]
+var tema=["France","Spain","Germany","Sweden","Denmark"]
+
+for (let i = 0; i< tema.length;i++){
+      let a = $("<button>")
+    a.addClass("newBut")
+    a.attr("data-type",tema[i])
+    a.text(tema[i])
+    $("#animal-buttons").append(a)
+ }
+
+$("#add-animal").on('click', addBut)
+
+function addBut(e){
+  e.preventDefault();
+  let item =$("#animal-input").val();
+  
+  if(item != ""){
+    $("#animal-buttons").append(`
+    <button id="newBut" data-type=${item}>
+    ${item}
+    `);
+}
+
+}
 
 
-$("#add-animal").on('click', addGif)
-
-function addGif(e){
+$("#animal-buttons").on("click", ".newBut", function(e){
     e.preventDefault();
-    let item =$("#animal-input").val();
+    let item =$(this).attr("data-type")
+    //let item = $('#newBut').attr('data-value');
     let peticion = {
         url:` https://api.giphy.com/v1/gifs/search?q=${item}&api_key=Pm36nbkA2E80pwabtZBC7GGT2c9K88MH&limit=10`,
         success: function (respuesta) {
           
-          let giflist = $("#animals")
+          let gifList = $("#animals")
           
           respuesta.data.forEach(element => {
-            giflist.append(` <div> 
-                              <img src = '${element.images.original.url}' /> 
-                              </div>`  )
+            gifList.append(`
+            <div> 
+            <p> ratng: ${element.rating} </p>
+            <img src = '${element.images.original.url}' /> 
+            </div>`  )
           })
           
           
@@ -32,49 +54,9 @@ function addGif(e){
       
       
       $.ajax(peticion)
-      
 
+})
 
-
-//     let item =$("#animal-input").val();
-//    // var query=`q=${item}`
-//     url:`https://api.giphy.com/v1/gifs/search?q=${item}&api_key=Pm36nbkA2E80pwabtZBC7GGT2c9K88MH&limit=10`
-//     let gifList=$(#animals)
-    
-
-
-}
-//     e.preventDefault();
-//     let item =$("#animal-input").val();
-
-//     let peticion = {
-//         url:'https://api.giphy.com/v1/gifs/search?api_key=Pm36nbkA2E80pwabtZBC7GGT2c9K88MH&limit=10',
-//         success: function (respuesta) {
-          
-//           let userlist = $("#animals")
-          
-//           respuesta.data.forEach(element => {
-//             userlist.append(` <div>  <p>  ${element.first_name} ${element.last_name} </p>
-//                               <img src = '${element.avatar}' /> 
-//                               </div>`  )
-//           })
-          
-          
-//         } ,
-//         error: function () {
-//          console.log("No se ha podido obtener la información")   
-//         }
-//       }
-      
-      
-//       $.ajax(peticion)    
-//     // if(item != ""){
-//     //     $("#animal-buttons").append(`
-//     //     <input id="butList" type="submit" value=${item}>
-//     //     `);
-//     // }
-
-// };
 
 // https://api.giphy.com/v1/gifs/search?q=dog&api_key=Pm36nbkA2E80pwabtZBC7GGT2c9K88MH&limit=10
 
